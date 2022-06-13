30418136fc1f0d22c7e0351797bf7b64d45fba76
Yura_viper <yuramcpecs@gmail.com>
2022-06-13 12:31:31 +0200

    Added link to AppLogo

--- src/components/Layout/Header/App_logo.js
+++ src/components/Layout/Header/App_logo.js
@@ -1,9 +1,12 @@
+import { NavLink } from 'react-router-dom';
 import ThemeSwitch from './Theme_switch';
 
 function AppLogo({ handleThemeChange }){
 	return(
 		<div id="appLogoContainer">
-			<img id="appLogo" src={require("../../../Files/Images/App_logo/logo.jpg")} alt="Our logo" />
+			<NavLink to='/'>
+				<img id="appLogo" src={require("../../../Files/Images/App_logo/logo.jpg")} alt="Our logo" />
+			</NavLink>
 		</div>
 	)
 }
