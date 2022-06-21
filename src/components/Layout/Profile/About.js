49c448e18449197a94658bd2a9a6f383b993a905
Yura_viper <yuramcpecs@gmail.com>
2022-06-21 07:44:09 +0200

    Some bug fixes

--- src/components/Layout/Profile/About.js
+++ src/components/Layout/Profile/About.js
@@ -1,9 +1,11 @@
 function About(){
     return(
         <div id="aboutWindow">
-            <p>Designed and developed by <b>YURA SHTEFANKO</b></p>
-            <p>Version: 1.0.5</p>
-            <p>Copyright 2009</p>
+            <div id="aboutWindowContent">
+                <p className="designedBy">Designed and developed by <br /> <b>YURA SHTEFANKO</b></p>
+                <p>Version: 1.0.5</p>
+                <p>Copyright 2009</p>
+            </div>
         </div>
     );
 }
